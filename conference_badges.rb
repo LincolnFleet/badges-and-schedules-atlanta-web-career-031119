33a3b05ce710030a08e8c78dc7e2068a37abf6fa
Travis Martin <travisemerymartin@gmail.com>
2019-03-02 23:53:15 +0000

    Done.

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,16 @@
 # Write your code here.
+def badge_maker(name)
+  badge = "Hello, my name is #{name}."
+  puts badge
+  return badge
+end
+
+speakers = [Edsger, Ada, Charles, Alan, Grace, Linus, Matz]
+
+def batch_badge_creator(array)
+  badge_messages = []
+  array.each {|name| badge_messages.push(badge_maker(name)}
+  return badge_messages
+end
+
+def assign_rooms(array)
